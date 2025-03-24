f522826e8ed29d92ee801872e0f9c4ed38d28127
Steponas Dauginis <steponas.dauginis@gmail.com>
2025-03-24 09:00:57 +0200

    Show the room code for others to be able to join

--- client/src/renderer/components/header/header.tsx
+++ client/src/renderer/components/header/header.tsx
@@ -10,13 +10,18 @@ export const Header = () => {
   return (
     <Box sx={{p: 2, background: blue[300], color: 'white'}}>
       <Stack justifyContent="space-between" direction="row" alignItems="center" spacing={2}>
-        <Typography
-          variant="h4"
-          sx={{whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden'}}
-        >
-          {ctx.currentUser.name}&apos;s
-          TODOs
-        </Typography>
+        <Stack justifyContent="space-between" direction="column" alignItems="flex-start">
+          <Typography
+            variant="h5"
+            sx={{whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden'}}
+          >
+            {ctx.currentUser.name}&apos;s
+            TODOs
+          </Typography>
+          <Typography variant="subtitle2" sx={{color: t => t.palette.grey[200]}}>
+            List code: {ctx.list?.code}
+          </Typography>
+        </Stack>
         <Button
           startIcon={<PlusOneIcon/>}
           variant="contained"
